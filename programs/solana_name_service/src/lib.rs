89e63968d4f0a640006a2c880144a3f34df32109
Saurish <saurishdarodkar@gmail.com>
2025-03-07 19:12:38 +0530

    Add instruction structure

--- programs/solana_name_service/src/lib.rs
+++ programs/solana_name_service/src/lib.rs
@@ -6,11 +6,61 @@ declare_id!("AMHL7gijZFDF6VKQzbT3TkuPSHmP76HP7KxBpV9RiG3y");
 pub mod solana_name_service {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, pk: Pubkey) -> Result<()> {
+        let store = &mut ctx.accounts.address_store;
+        store.address = pk;
+        store.owner = ctx.accounts.user.key();
+        msg!("PDA initialized with value {}", pk);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(seed: String)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(  
+        init_if_needed,
+        payer=user,
+        space=20,
+        seeds=[seed.as_bytes()],
+        bump
+    )]
+    pub address_store: Account<'info, AddressStore>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(seed: String)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=[seed.as_bytes()],
+        bump
+    )]
+    pub address_store: Account<'info, AddressStore>,
+}
+
+#[derive(Accounts)]
+#[instruction(seed: String)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=[seed.as_bytes()],
+        bump
+    )]
+    pub address_store: Account<'info, AddressStore>,
+}
+#[account]
+pub struct AddressStore{
+    pub owner: Pubkey,
+    pub address: Pubkey 
+}
